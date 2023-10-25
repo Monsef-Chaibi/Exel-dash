f2017a0fa904baaa3e88b36abe800816b8621157
Monsef-Chaibi <xr.monsef@gmail.com>
2023-10-25 10:59:22 +0100

    v122

--- app/Imports/DataImport.php
+++ app/Imports/DataImport.php
@@ -14,21 +14,30 @@ class DataImport implements ToModel
     */
     private $rowCount = 0;
     public function model(array $row)
-{
-    $this->rowCount++;
+    {
+        Data::where('vu', 0)->delete();
+        $this->rowCount++;
 
-    if ($this->rowCount >= 22 && $row[9] !== 'GT TO ISTIMARAH') {
-        return new Data([
-            "product"=> $row[4],
-            "desc"=> $row[5],
-            "gtnum"=>$row[8],
-            "plantkey"=>$row[1],
-            "soldp"=> $row[13],
-            "shipp"=> $row[14],
-            "bildoc"=> $row[19],
-            "bildt"=> $row[20],
-        ]);
+        if ($this->rowCount >= 22 && $row[9] !== 'GT TO ISTIMARAH') {
+
+            // Check if 'gtnum' and 'bild' already exist
+            $existingRecord = Data::where('gtnum', $row[8])
+                                  ->where('bildoc', $row[19])
+                                  ->first();
+
+            if (!$existingRecord) {
+                return new Data([
+                    "product"=> $row[4],
+                    "desc"=> $row[5],
+                    "gtnum"=>$row[8],
+                    "plantkey"=>$row[1],
+                    "soldp"=> $row[13],
+                    "shipp"=> $row[14],
+                    "bildoc"=> $row[19],
+                    "bildt"=> $row[20],
+                ]);
+            }
+        }
+        return null;
     }
-    return null;
-}
 }
