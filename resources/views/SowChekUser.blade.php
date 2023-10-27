c2aec350017360e490b44908d27325ac72f8f2e3
Monsef-Chaibi <xr.monsef@gmail.com>
2023-10-27 18:22:50 +0100

    v12

--- resources/views/SowChekUser.blade.php
+++ resources/views/SowChekUser.blade.php
@@ -284,7 +284,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <form action="">
+            <form action="/SemiExport" method="GET">
 
             <button type="submit" class="btn" style="">Export Exel +
             </button>
