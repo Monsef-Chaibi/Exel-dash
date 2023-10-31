e91f36938349746712396a76d07924c2b5a6a7d0
Monsef-Chaibi <xr.monsef@gmail.com>
2023-10-31 11:10:28 +0100

    v12

--- resources/views/DashboardB.blade.php
+++ resources/views/DashboardB.blade.php
@@ -318,7 +318,7 @@
                         <div class="ag-courses_box">
 
                             <div class="ag-courses_item">
-                                <a href="#" class="ag-courses-item_link">
+                                <a href="/notcheck" class="ag-courses-item_link">
                                     <div class="ag-courses-item_bg"></div>
 
                                     <div class="ag-courses-item_title">
