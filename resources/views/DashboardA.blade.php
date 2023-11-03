f32aeb6673ba3fe6795457b3bee3ad81a1ca12f3
Monsef-Chaibi <xr.monsef@gmail.com>
2023-11-03 13:43:35 +0100

    v12

--- resources/views/DashboardA.blade.php
+++ resources/views/DashboardA.blade.php
@@ -5,10 +5,6 @@
         </h2>
     </x-slot>
     <style>
-        .ag-format-container {
-  width: 1142px;
-  margin: 0 auto;
-}
 .box {
   display: flex;
   flex-direction: column;
@@ -16,6 +12,7 @@
   align-items: center;
   height: 100vh;
   padding: 0 4rem 2rem;
+  background-color: white
 }
 
 .box:not(:first-child) {
@@ -80,7 +77,7 @@ when users will click/enter button(link) browser will add a #id in a url and whe
   border-radius: .8rem;
 
   color: var(--light);
-  background: var(--background);
+  background-color: red;
   box-shadow: var(--m-shadow, .4rem .4rem 10.2rem .2rem) var(--shadow-1);
   position: relative;
 
@@ -193,6 +190,11 @@ when users will click/enter button(link) browser will add a #id in a url and whe
   color: hsla(0, 0%, 1000%, .6);
   font-size: 1.6rem;
 }
+        .ag-format-container {
+  width: 1142px;
+  margin: 0 auto;
+}
+
 
 body {
   background-color: #000;
@@ -473,7 +475,7 @@ body {
                     <div class="ag-courses_box" >
 
                         <div class="ag-courses_item">
-                            <a href="#" class="ag-courses-item_link">
+                            <a  href="#m1-o"  class="ag-courses-item_link">
                                 <div class="ag-courses-item_bg"></div>
 
                                 <div style="text-align: center;" class="ag-courses-item_title">
@@ -562,18 +564,8 @@ body {
     </script>
 
 </x-app-layout>
-<!-- title -->
-<div class="box">
-    <h1 class="box__title">Modals</h1>
-    <p class="box__p">Click / Enter the buttons to open modals.</p>
-  </div>
-  <!-- /title -->
-
-
   <!-- modal 1 -->
   <div class="box">
-    <a href="#m1-o" class="link-1" id="m1-c">Modal 1</a>
-    <p class="box__info">Without Background</p>
 
     <div class="modal-container" id="m1-o" style="--m-background: transparent;">
       <div class="modal">
@@ -585,48 +577,3 @@ body {
     </div>
   </div>
   <!-- /modal 1 -->
-
-
-  <!-- modal 2 -->
-  <div class="box">
-    <a href="#m2-o" class="link-1" id="m2-c">Modal 2</a>
-    <p class="box__info">With Background (Opacity .4)</p>
-
-    <div class="modal-container" id="m2-o" style="--m-background: hsla(0, 0%, 0%, .4);">
-      <div class="modal">
-        <h1 class="modal__title">Modal 2 Title</h1>
-        <p class="modal__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ex dicta maiores libero minus obcaecati iste optio, eius labore repellendus.</p>
-        <button class="modal__btn">Button &rarr;</button>
-        <button class="modal__btn">Button &rarr;</button>
-        <a href="#m2-c" class="link-2"></a>
-      </div>
-    </div>
-  </div>
-  <!-- /modal 2 -->
-
-
-  <!-- modal 3 -->
-  <div class="box">
-    <a href="#m3-o" class="link-1" id="m3-c">Modal 3</a>
-    <p class="box__info">With Background Full Opacity</p>
-
-    <div class="modal-container" id="m3-o" style="--m-background: var(--global-background);">
-      <div class="modal" style="--m-shadow: 0 0 10rem 0">
-        <h1 class="modal__title">Modal 3 Title</h1>
-        <p class="modal__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ex dicta maiores libero minus obcaecati iste optio, eius labore repellendus.</p>
-        <button class="modal__btn">Button &rarr;</button>
-        <button class="modal__btn">Button &rarr;</button>
-        <a href="#m3-c" class="link-2"></a>
-      </div>
-    </div>
-  </div>
-  <!-- /modal 3 -->
-
-
-  <!-- Note(s) -->
-  <div class="box">
-    <p class="box__note">For users using a mouse, they have to close the modal to be able to click on something else, but for users using a keyboard, they can navigate outside of the modal (when modal is opened). Still need JavaScript for certain <em>things</em> but the pen is about what we can achieve using CSS only, so... and about why I gave the open modal link a unique id(m$-c) is because, when our users will click/enter the close link, I want to send them back to that open link rather than sending them on the top of the page(by setting href="#"). <em>Might</em> be not an issue for users using a mouse <em>(if I am the user, yes it definitely is)</em>, but for users using the keyboard, it definitely is; me just preventing that.</p>
-  </div>
-  <!-- /Note(s) -->
-
-  <a href="https://abubakersaeed.netlify.app/designs/d8-modals" class="abs-site-link" rel="nofollow noreferrer" target="_blank">abs/designs/d8-modals</a>
