82623c4edd16f52f348171bb7b13e5a515d68ea2
Monsef-Chaibi <xr.monsef@gmail.com>
2023-11-08 11:04:23 +0100

    v12

---
+++ database/migrations/2023_11_08_100144_create_contrat_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contrat_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('nat')->nullable();
+            $table->string('nat_id')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('wornum')->nullable();
+            $table->string('mobnum')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contrat_users');
+    }
+};
