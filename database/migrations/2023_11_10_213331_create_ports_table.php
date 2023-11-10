9ad07ecb7be5aac785f0da12edbacd28bcbf0f44
Monsef-Chaibi <xr.monsef@gmail.com>
2023-11-10 22:59:47 +0100

    v12

--- database/migrations/2023_11_10_213331_create_ports_table.php
+++ database/migrations/2023_11_10_213331_create_ports_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('ports', function (Blueprint $table) {
             $table->id();
+            $table->string('nameofport')->nullable();
             $table->timestamps();
         });
     }
