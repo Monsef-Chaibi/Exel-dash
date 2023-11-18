cdffd6f0cbd1b5d2e629ff84fc1657e79fc49ff4
Monsef-Chaibi <xr.monsef@gmail.com>
2023-11-18 22:59:05 +0100

    v12

--- resources/views/DashboardC.blade.php
+++ resources/views/DashboardC.blade.php
@@ -322,25 +322,33 @@
 </x-app-layout>
 <script>
     document.addEventListener('DOMContentLoaded', function () {
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            type: 'bar',
+        var ctx = document.getElementById('myDoughnutChart').getContext('2d');
+        var doughnutChart = new Chart(ctx, {
+            type: 'doughnut',
             data: {
                 labels: {!! json_encode($Data->pluck('label')) !!},
                 datasets: [{
-                    label: 'Chart Data',
                     data: {!! json_encode($Data->pluck('value')) !!},
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.7)',
+                        'rgba(54, 162, 235, 0.7)',
+                        'rgba(255, 206, 86, 0.7)',
+                        'rgba(75, 192, 192, 0.7)',
+                        'rgba(153, 102, 255, 0.7)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                    ],
                     borderWidth: 1
                 }]
             },
             options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
+                responsive: true,
+                maintainAspectRatio: false,
             }
         });
     });
