23bf64928ab870ac184978ecead12fbc474d9072
Monsef-Chaibi <xr.monsef@gmail.com>
2023-11-18 22:40:08 +0100

    v12

--- resources/views/DashboardC.blade.php
+++ resources/views/DashboardC.blade.php
@@ -311,7 +311,11 @@
     </style>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <div class="py-12">
-        <h1>User C</h1>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                
+            </div>
+        </div>
     </div>
 
 </x-app-layout>
