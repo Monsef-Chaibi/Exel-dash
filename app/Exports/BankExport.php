04131d7716868a8ca7ffda0b61807fa5b022b62d
Monsef-Chaibi <xr.monsef@gmail.com>
2023-12-03 00:25:04 +0100

    v12

--- app/Exports/BankExport.php
+++ app/Exports/BankExport.php
@@ -1,38 +1,16 @@
 <?php
 
-namespace App\Exports;
+require_once 'PHPExcel/IOFactory.php';
 
-use App\Models\Data;
-use Maatwebsite\Excel\Concerns\FromCollection;
+// Load the existing Excel file
+$objPHPExcel = PHPExcel_IOFactory::load('path/to/your/excel/file.xlsx');
 
-class BankExport implements FromCollection
-{
-    protected $selectedItems;
-    protected $alldata;
+// Access the desired worksheet
+$worksheet = $objPHPExcel->getActiveSheet();
 
-    public function __construct($selectedItems, $alldata)
-    {
-        $this->selectedItems = $selectedItems;
-        $this->alldata = $alldata;
-    }
+// Modify the worksheet data
+$worksheet->setCellValue('A1', 'Updated value');
 
-    public function collection()
-    {
-        // Use the $this->selectedItems and $this->alldata to fetch the actual data you want to export
-        $data = $this->fetchData(); // Replace with your logic to fetch data
-
-        // Return the data as a collection
-        return collect($data);
-    }
-
-    // Replace this method with your actual logic to fetch data
-    protected function fetchData()
-    {
-        // Your logic to fetch and prepare the data
-        // For example, you might fetch data from the database based on $this->selectedItems and $this->alldata
-
-        // Assuming you have a Data model and want to fetch data from the database
-        // Replace 'column1', 'column2', etc., with the actual column names in your database table
-        return Data::whereIn('id', $this->selectedItems)->get(['id'])->toArray();
-    }
-}
+// Save the updated Excel file
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter->save('path/to/updated/excel/file.xlsx');
