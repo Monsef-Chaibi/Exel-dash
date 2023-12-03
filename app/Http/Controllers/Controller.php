be57e6fd237d9ba5bf5d243b73face606174eb88
Monsef-Chaibi <xr.monsef@gmail.com>
2023-12-03 16:39:22 +0100

    v12

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,12 +9,10 @@ use Maatwebsite\Excel\Validators\ValidationException;
 use Symfony\Component\Debug\Exception\FatalThrowableError;
 use Symfony\Component\Debug\Exception\OutOfMemoryException;
 use App\Exports\DataExport;
-use App\Exports\BankExport;
 use App\Exports\DataSemiExport;
 use App\Imports\DataImport;
 use App\Imports\IDImport;
 use App\Imports\Sadad;
-use Illuminate\Support\Facades\Storage;
 use App\Imports\HSBCImport;
 use App\Models\Brand;
 use App\Models\ContratUser;
@@ -1201,22 +1199,14 @@ class Controller extends BaseController
                         $firstId = $selectedItems[0];
                         $firstRecord = Data::find($firstId);
                         $alldata = $firstRecord->paidtype;
-
                         if (!$selectedItems) {
                             throw new \Exception('No items selected for export.');
                         }
 
-                        $export = new BankExport($selectedItems, $alldata);
-
-                        // Use the store method to store the file and get the path
-                        $filePath = $export->store('exports', 'public'); // Change 'exports' to your desired directory
-
-                        // You can now provide a link to the stored file using the $filePath
-                        return redirect()->away(Storage::url($filePath));
+                        return Excel::download(new DataSemiExport($selectedItems, $alldata), 'Gt-Number.xlsx');
                     } catch (\Exception $e) {
                         return back()->with('error', 'An error occurred while exporting the data: ' . $e->getMessage());
                     }
-
                 }
                 else{
                     try {
